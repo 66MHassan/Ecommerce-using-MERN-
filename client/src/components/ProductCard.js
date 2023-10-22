4bf0547f617625926741ba94583896a5d6739db4
66MHassan <muhammadhassan662000@gmail.com>
2023-10-22 23:02:30 +0500

    Added ProductCard component

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CurrencyFormater from './CurrencyFormater';
+
+const ProductCard = (data) => {
+    const{id,name,image,price,category}=data;
+  return (
+    <Link to={`/single/${id}`}>
+    <div className="container">
+  <div className="card out" style={{height:"auto"}}>
+    <img src={image} className='img-fluid' alt="Card Image" /> 
+    <div className="card-body">
+      <p className="card-title">{name}</p>
+      <p className='card-text'>price:{<CurrencyFormater price={price} />}</p>
+
+    </div>
+  </div>
+</div>
+</Link>
+  )
+}
+
+export default ProductCard
